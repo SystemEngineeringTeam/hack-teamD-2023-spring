f6784f38848d9a1afb17a61a646214e6d7cad650
nakayama900 <49431394+nakayama900@users.noreply.github.com>
2023-03-16 13:34:41 +0900

    feat-fetch: uri builder

--- Assets/Code/Fetch.cs
+++ Assets/Code/Fetch.cs
@@ -2,35 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-
+using System.UriBuilder;
 public class Fetch
 {
-    IEnumerator ASyncQ(string q)
-    {
-        UnityWebRequest webRequest = UnityWebRequest.Get(url);
-        yield return webRequest.SendWebRequest();
-        if (webRequest.isNetworkError){
-            //通信失敗
-            Debug.Log(webRequest.error);
-        }
-        else
-        {
-            //通信成功
-            //ZipクラスにJSONデータを格納する
-            Zip zip = JsonUtility.FromJson<Zip>(webRequest.downloadHandler.text);
-            //zipクラスに格納したJSONデータをゲームオブジェクトUI > Textに出力する
-            textResult.text = zip.message + "," + zip.results.Length + "," + zip.status;
-            foreach (ZipResult zr in zip.results)
-            {
-                textResult.text += string.Format("\n{0},{1},{2},{3}", zr.address1, zr.address2, zr.address3, zr.prefcode);
-            }
-        }
-    }
 
     public string[] SyncQ(string[] q)
     {
+    }
+
+    private string baseurl = "/";
+        var query = System.Web.HttpUtility.ParseQueryString("");
+        for(string i in q){
+            query.Add("q[]", i);
+        }
+        UriBuilder ub = new UriBuilder(
+            "http", baseurl, 80, "guest", query.ToString());
+        private string url = ub.ToString()
         UnityWebRequest webRequest = UnityWebRequest.Get(url);
-        yield return webRequest.SendWebRequest();
+        webRequest.SendWebRequest();
         if (webRequest.isNetworkError){
             //通信失敗
             Debug.Log(webRequest.error);
